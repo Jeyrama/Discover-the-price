932393a3cf5a5ff32748eee4f3858b3751c88a13
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-09-21 16:42:01 -0700

    Update Discover-the-price.js

--- Discover-the-price.js
+++ Discover-the-price.js
@@ -13,3 +13,6 @@ For example:
   DiscoverOriginalPrice(75, 25) => 100.00M where 75 is the sale price (discounted price), 
   25 is the sale percentage and 100 is the original price.
 */
+
+
+// Solution
